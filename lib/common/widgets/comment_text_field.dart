49572383a6cd1ef23454a30ded0589eccedfb8a8
NykolayR <nykolayr@gmail.com>
2024-06-29 13:33:57 +0700

    fix: comments

--- lib/common/widgets/comment_text_field.dart
+++ lib/common/widgets/comment_text_field.dart
@@ -2,7 +2,6 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
-import 'package:flutter_keyboard_visibility/flutter_keyboard_visibility.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:poster_stock/common/services/text_info_service.dart';
 import 'package:poster_stock/common/widgets/app_text_button.dart';
@@ -29,6 +28,7 @@ class _CommentTextFieldState extends ConsumerState<CommentTextField> {
   final GlobalKey key = GlobalKey();
   final TextEditingController controller = TextEditingController();
   bool loading = false;
+  bool isDisable = true;
 
   @override
   Widget build(BuildContext context) {
@@ -39,22 +39,18 @@ class _CommentTextFieldState extends ConsumerState<CommentTextField> {
       onNotification: (not) {
         return true;
       },
-      child: Column(
-        children: [
-          Divider(
-            height: 0.5,
-            thickness: 0.5,
-            color: context.colors.fieldsDefault,
-          ),
-          Container(
-            height:
-                focus.hasFocus && MediaQuery.of(context).viewInsets.bottom != 0
-                    ? null
-                    : 56 + MediaQuery.of(context).padding.bottom,
-            color: context.colors.backgroundsPrimary,
-            child: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 16.0),
-              child: TextField(
+      child: SafeArea(
+        child: Container(
+          padding: const EdgeInsets.symmetric(horizontal: 16.0),
+          color: context.colors.backgroundsPrimary,
+          child: Column(
+            children: [
+              Divider(
+                height: 0.5,
+                thickness: 0.5,
+                color: context.colors.fieldsDefault,
+              ),
+              TextField(
                 key: key,
                 maxLines: null,
                 focusNode: focus,
@@ -65,96 +61,100 @@ class _CommentTextFieldState extends ConsumerState<CommentTextField> {
                   border: InputBorder.none,
                   hintText:
                       AppLocalizations.of(context)!.poster_reply_field_hint,
-                  hintStyle: context.textStyles.callout!.copyWith(
+                  hintStyle: context.textStyles.bodyRegular!.copyWith(
                     color: context.colors.textsDisabled,
                   ),
                 ),
                 textCapitalization: TextCapitalization.sentences,
-                onChanged: (value) {
+                onChanged: (String text) => setState(() {
+                  if (text.isEmpty) {
+                    isDisable = true;
+                  } else {
+                    isDisable = false;
+                  }
                   setState(() {});
-                },
+                }),
               ),
-            ),
-          ),
-          if (focus.hasFocus)
-            KeyboardVisibilityBuilder(builder: (context, visible) {
-              if (!visible) return const SizedBox();
-              return Container(
-                height: 56,
-                color: context.colors.backgroundsPrimary,
-                child: Row(
-                  children: [
-                    const Spacer(),
-                    Text(
-                      '${controller.text.length}/140',
-                      style: context.textStyles.footNote!.copyWith(
-                        color: controller.text.length > 140
-                            ? context.colors.textsError
-                            : context.colors.textsDisabled,
+              if (!isDisable)
+                Container(
+                  height: 56,
+                  color: context.colors.backgroundsPrimary,
+                  child: Row(
+                    children: [
+                      const Spacer(),
+                      Text(
+                        '${controller.text.length}/140',
+                        style: context.textStyles.footNote!.copyWith(
+                          color: controller.text.length > 140
+                              ? context.colors.textsError
+                              : context.colors.textsDisabled,
+                        ),
                       ),
-                    ),
-                    const SizedBox(width: 12),
-                    SizedBox(
-                      height: 32,
-                      width: TextInfoService.textSize(
-                                  AppLocalizations.of(context)!.poster_reply,
-                                  context.textStyles.calloutBold!)
-                              .width +
-                          32,
-                      child: AppTextButton(
-                        text: AppLocalizations.of(context)!.poster_reply,
-                        disabled: controller.text.isEmpty ||
-                            controller.text.length > 140,
-                        child: loading
-                            ? Center(
-                                child: defaultTargetPlatform !=
-                                        TargetPlatform.android
-                                    ? CupertinoActivityIndicator(
-                                        radius: 10.0,
-                                        color: context.colors.textsBackground!,
-                                      )
-                                    : SizedBox(
-                                        width: 16,
-                                        height: 16,
-                                        child: CircularProgressIndicator(
+                      const SizedBox(width: 12),
+                      SizedBox(
+                        height: 32,
+                        width: TextInfoService.textSize(
+                                    AppLocalizations.of(context)!.poster_reply,
+                                    context.textStyles.calloutBold!)
+                                .width +
+                            32,
+                        child: AppTextButton(
+                          text: AppLocalizations.of(context)!.poster_reply,
+                          disabled: controller.text.isEmpty ||
+                              controller.text.length > 140,
+                          child: loading
+                              ? Center(
+                                  child: defaultTargetPlatform !=
+                                          TargetPlatform.android
+                                      ? CupertinoActivityIndicator(
+                                          radius: 10.0,
                                           color:
                                               context.colors.textsBackground!,
-                                          strokeWidth: 2,
+                                        )
+                                      : SizedBox(
+                                          width: 16,
+                                          height: 16,
+                                          child: CircularProgressIndicator(
+                                            color:
+                                                context.colors.textsBackground!,
+                                            strokeWidth: 2,
+                                          ),
                                         ),
-                                      ),
-                              )
-                            : null,
-                        onTap: () async {
-                          loading = true;
-                          setState(() {});
-                          if (!widget.isList) {
-                            await ref
-                                .read(postControllerProvider)
-                                .postComment(widget.id, controller.text);
-                            await ref
-                                .read(homePagePostsControllerProvider)
-                                .addComment(widget.id);
-                          } else {
-                            await ref
-                                .read(postControllerProvider)
-                                .postCommentList(widget.id, controller.text);
-                            await ref
-                                .read(homePagePostsControllerProvider)
-                                .addCommentList(widget.id);
-                          }
-                          loading = false;
-                          setState(() {});
-                          controller.clear();
-                          focus.unfocus();
-                        },
+                                )
+                              : null,
+                          onTap: () async {
+                            loading = true;
+                            setState(() {});
+                            if (!widget.isList) {
+                              await ref
+                                  .read(postControllerProvider)
+                                  .postComment(widget.id, controller.text);
+                              await ref
+                                  .read(homePagePostsControllerProvider)
+                                  .addComment(widget.id);
+                            } else {
+                              await ref
+                                  .read(postControllerProvider)
+                                  .postCommentList(widget.id, controller.text);
+                              await ref
+                                  .read(homePagePostsControllerProvider)
+                                  .addCommentList(widget.id);
+                            }
+                            loading = false;
+                            isDisable = true;
+                            setState(() {});
+                            controller.clear();
+                            focus.unfocus();
+                          },
+                        ),
                       ),
-                    ),
-                    const SizedBox(width: 16),
-                  ],
+                      const SizedBox(width: 16),
+                    ],
+                  ),
                 ),
-              );
-            })
-        ],
+            ],
+          ),
+        ),
       ),
     );
   }
